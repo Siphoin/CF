48bc30a8d27e0741391219ca7b7910a846953cdd
Siphoin <maksimka.kosolapov.01@mail.ru>
2021-06-23 19:41:56 +0500

    add coroutines helper

---
+++ Csstle Fight/Assets/Scripts/Others/Coroutines.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Coroutines
+{
+    public sealed class Coroutines : MonoBehaviour
+    {
+        private static Coroutines m_instance = null;
+        private static Coroutines Instance { get
+            {
+                if (!m_instance)
+                {
+                    var obj = new GameObject("Coroutines Manager");
+                    m_instance = obj.AddComponent<Coroutines>();
+                    DontDestroyOnLoad(obj);
+                }
+
+
+                return m_instance;
+            }
+        
+        }
+
+        public static Coroutine StartRoutine (IEnumerator routine)
+        {
+            return Instance.StartCoroutine(routine);
+        }
+
+        public static void StopRoutine (Coroutine coroutine)
+        {
+            if (Instance)
+            {
+                Instance.StopCoroutine(coroutine);
+            }
+        }
+    }
+}
